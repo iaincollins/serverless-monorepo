28c114dee82303f9b3b0ecd0cd45abcb4c01e27d
Iain Collins <iaincollins@economist.com>
2019-02-25 01:31:46 +0000

    Improve React code and comments

--- www/pages/test.js
+++ www/pages/test.js
@@ -2,17 +2,22 @@ import React from "react";
 import Page from "../components/page";
 import 'isomorphic-unfetch'
 
+async function callApi(url) {
+  const request = await fetch(url)
+  return await request.json()
+}
+
 export default class extends React.Component {
+  // getInitialProps() runs server side on first run and client side on subsequent page loads
   static async getInitialProps({ req }) {
+    // The request object `req` only exists server side and `window` is only client side
+    // When in development mode, host is always http://localhost:3001
     const server = (process.env.NODE_ENV === 'production') 
-      // When running on server get hostname from `req` when in client use `window`
       ? (req) ? `https://${req.headers.host}` : `https://${window.location.host}`
-      // Development is always http://localhost:3001
       : 'http://localhost:3001'
 
-    const request = await fetch(`${server}/api/test`)
-    const json = await request.json()
-    return { message: json.testMessage }
+    const apiData = await callApi(`${server}/api/test`)
+    return { message: apiData.testMessage }
   }
 
   constructor(props) {
@@ -20,19 +25,16 @@ export default class extends React.Component {
     this.state = {
       message: props.message
     }
-    this.onCallApiEndpoint = this.onCallApiEndpoint.bind(this)
+    this.onUpdateMessage = this.onUpdateMessage.bind(this)
   }
 
-  async onCallApiEndpoint() {
-    // This can only be called from client side
+  async onUpdateMessage() {
+    // This function is bound to a button onClick and can only be called client side
     const server = (process.env.NODE_ENV === 'production') 
     ? `https://${window.location.host}`
     : 'http://localhost:3001'
-    const request = await fetch(`${server}/api/test`)
-    const json = await request.json()
-    this.setState({
-      message: json.testMessage
-    })
+    const apiData = await callApi(`${server}/api/test`)
+    this.setState({ message: apiData.testMessage })
   }
 
   render() {
@@ -40,14 +42,16 @@ export default class extends React.Component {
     return (
       <Page>
         <h1>Serverless Monorepo</h1>
-        <p>This is an example of a React frontend (using 'Next.js') and an API (using 'micro') where both the frontend and backend are serverless.</p>
-        <h3>Data from the endpoint /api/test</h3>
+        <p>This is an example of a monorepo with a React frontend and a backend API, where both are serverless.</p>
+        <h3>Example</h3>
+        <p>Data from the endpoint at /api/test :</p>
         <pre style={{border: '1px solid black', padding: 5, display: 'inline-block'}}>
           {message}
         </pre>
         <p>On initial page render the data is fetched using server side logic.</p>
-        <p>If you click the button below, it will be fetched client side.</p>
-        <button onClick={this.onCallApiEndpoint}>Call API From Client</button>
+        <p>If you click the button below, it will call the API from the browser (client side).</p>
+        <button onClick={this.onUpdateMessage}>Call API From Client</button>
+        <hr/>
         <p>
           View source on <a href="https://github.com/iaincollins/serverless-monorepo">github.com/iaincollins/serverless-monorepo</a>
         </p>
